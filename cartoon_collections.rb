e3a4d38dc9a418ac0471c444c9a1e3a1b8f9ecb8
Jay Bednar <ironviralmedia@gmail.com>
2018-02-07 02:12:30 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,27 +1,27 @@
 def ROLL_CALL_DWARVES(array) 
   array.each_with_index do |dwarf, i|
-    puts "#{i + 1} #{dwarf}" 
-  end 
-end 
+    puts "#{i + 1} #{dwarf}"
+  end
+end
 
 #ROLL_CALL_DWARVES(["Doc", "Dopey", "Bashful", "Grumpy"])
 
 def SUMMON_CAPTAIN_PLANET(array)
   array.map do |call|
     call.capitalize + "!"
-  end 
-end 
+  end
+end
 
 #SUMMON_CAPTAIN_PLANET(["earth", "wind", "fire", "water", "heart"])
 
-def LONG_PLANETEER_CALLS(array) 
+def LONG_PLANETEER_CALLS(array)
   array.any? {|call| call.length > 4}
-end 
+end
 
 #LONG_PLANETEER_CALLS(["puff", "go", "two"])
 
 def FIND_THE_CHEESE(array)
   array.detect {|cheese| cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"}
-end 
+end
 
 #FIND_THE_CHEESE(["crackers", "camembert", "thyme"])
